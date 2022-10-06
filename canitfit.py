6d683c957a176703dade89ca5f60cb70d5123572
Maroko1218 <markus.svhn@gmail.com>
2022-10-06 09:31:42 +0200

    Added constants for easily changing simulation parameters. Added more list expressions compared to full on for loops.

--- canitfit.py
+++ canitfit.py
@@ -1,6 +1,17 @@
 from math import ceil
 from random import randint
 
+SIMULATIONS = 100000 #Amount of times tasks will be randomly created and checked against my method and the real one
+
+TASKS_LOWER_LIMIT = 1 #The amount of tasks is randomly generated between the lower and upper limit
+TASKS_UPPER_LIMIT = 10
+
+EXECUTION_TIME_LOWER_LIMIT = 1 #Execution time for each individual task is randomly generated between the lower and upper limit
+EXECUTION_TIME_UPPER_LIMIT = 500
+
+PERIOD_TIME_LOWER_LIMIT = 1 #Period time for each individual task is randomly generated between the lower and upper limit
+PERIOD_TIME_UPPER_LIMIT = 500
+
 class Task:
     def __init__(self, C, T):
         self.c = C
@@ -11,16 +22,14 @@ def firstCalculation(arr):
 
 def repeatCalculation(arr, specialTask, value, count=1):
     total = specialTask.c
-    for task in arr:
-        total += ceil(value/task.t) * task.c
+    total += sum(ceil(value/t.t) * t.c for t in arr)
     if value == total or total >= specialTask.t:
         return True if total <= specialTask.t else False, count
     return repeatCalculation(arr, specialTask, total, count+1)
 
 def markusTheorem(arr, specialTask):
     total = specialTask.c
-    for task in arr:
-        total += ceil(specialTask.t/task.t) * task.c
+    total += sum(ceil(specialTask.t/t.t) * t.c for t in arr)
     if total <= specialTask.t:
         return True
     return False
@@ -28,11 +37,11 @@ def markusTheorem(arr, specialTask):
 def main():
     markuscount = 0
     wastedcount = 0
-    possiblesavedcalculations = 0
-    for _ in range(100000):
+    potentiallyskippedcalculations = 0
+    for _ in range(SIMULATIONS):
         arr = []
-        for i in range(randint(1, 10)):
-            arr.append(Task(randint(1, 500), randint(1, 500)))
+        for i in range(randint(TASKS_LOWER_LIMIT, TASKS_UPPER_LIMIT)):
+            arr.append(Task(randint(EXECUTION_TIME_LOWER_LIMIT, EXECUTION_TIME_UPPER_LIMIT), randint(PERIOD_TIME_LOWER_LIMIT, PERIOD_TIME_UPPER_LIMIT)))
         biggest = max([t.t for t in arr])
         for i, task in enumerate(arr):
             if task.t == biggest:
@@ -42,12 +51,12 @@ def main():
         real, count = repeatCalculation(arr, biggest, firstCalculation(arr))
         if markus:
             markuscount += 1
-            possiblesavedcalculations += count - 1 #The -1 is because we're adding 1 calculation to check the upper bound
+            potentiallyskippedcalculations += count - 1 #The -1 is because we're adding 1 calculation by testing my method
         if real and not markus:
             wastedcount += 1
-        if markus and not real:
+        if markus and not real: #If this outcome would ever be true my Theorem falls apart
             print("Markus has said this should be valid but it wasn't so it's proven false")
-    print("Times the full calculation could be skipped:", markuscount, "\nTimes my theorem lead to no answer:", wastedcount, "\nCalculation steps that could've been skipped:", possiblesavedcalculations)
+    print("Times the full calculation could be skipped:", markuscount, "\nTimes my theorem lead to no answer:", wastedcount, "\nCalculation steps that could've been skipped:", potentiallyskippedcalculations)
 
 if __name__ == "__main__":
     main()
